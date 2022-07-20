fa9be8fca01a8de74abdb7439369edc5756afffb
Anudeep-47 <83406607+Anudeep-47@users.noreply.github.com>
2022-07-20 12:26:31 -0400

    Time: 4 ms (99.17%), Space: 112.9 MB (81.22%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,19 +1,42 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        if(points.length==k)return points;
-        Queue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0]-a[0]);
-        for(int i=0; i<points.length; i++){
-            int sqrdDis = points[i][0]*points[i][0] + points[i][1]*points[i][1];
-            maxHeap.add(new int[]{sqrdDis, i});
-            if(maxHeap.size() > k){
-                maxHeap.poll();
+        int low = 0;
+        int high = points.length-1;
+        int pivotIndex = points.length;
+        while(pivotIndex!=k){
+            pivotIndex = partition(points, low, high);
+            if(pivotIndex<k){
+                low = pivotIndex;
+            }
+            else{
+                high = pivotIndex-1;
             }
         }
-        int res[][] = new int[k][2];
-        while(!maxHeap.isEmpty()){
-            int index = maxHeap.poll()[1];
-            res[--k] = points[index];
-        }
-        return res;
+        return Arrays.copyOf(points, k);
     }
+    
+    private int partition(int[][] points, int low, int high){
+        int[] pivotPoint = points[low+(high-low)/2];
+        int pivotDis = sqrdDis(pivotPoint);
+        while(low<high){
+            if(sqrdDis(points[low])>=pivotDis){
+                int[] temp = points[low];
+                points[low] = points[high];
+                points[high] = temp;
+                high--;
+            }
+            else {
+                low++;
+            }
+        }
+        if(sqrdDis(points[low])<pivotDis){
+            low++;
+        }
+        return low;
+    }
+    
+    private int sqrdDis(int[] point){
+        return point[0]*point[0]+point[1]*point[1];
+    }
+    
 }
