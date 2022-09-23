fd76273dc0e17aeaab3bd514e7d47fc6c3ec0a5d
Anudeep-47 <83406607+Anudeep-47@users.noreply.github.com>
2022-09-23 03:08:36 -0400

    Time: 1 ms (41.14%), Space: 47.7 MB (9.57%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,32 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int starting = binarySearch(nums, target, true);
-        if(starting==-1){
-            return new int[]{-1,-1};
+        if(nums.length==0)return new int[]{-1, -1};
+        int start = findFirstOccurance(nums, target, 0, nums.length-1);
+        int end = -1;
+        if(start!=end){
+            end = findLastOccurance(nums, target, 0, nums.length-1);
         }
-        int ending = binarySearch(nums, target, false);
-        return new int[]{starting, ending};
+        return new int[]{start, end};
     }
-    
-    private int binarySearch(int[] nums, int target, boolean first){
-        int low=0, high=nums.length-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(nums[mid]<target){
-                low = mid+1;
+    private int findFirstOccurance(int[] nums, int target, int start, int end){
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]>=target){
+                end = mid;
             }
-            else if(nums[mid]>target){
-                high = mid-1;
-            }
-            else {
-                if(first){
-                    if(mid==low || nums[mid-1]!=target){
-                        return mid;
-                    }
-                    high = mid-1;
-                }
-                else {
-                    if(mid==high || nums[mid+1]!=target){
-                        return mid;
-                    }
-                    low = mid+1;
-                }
-            }
+            else start = mid+1;
         }
+        if(nums[start]==target)return start;
         return -1;
+    }
+    private int findLastOccurance(int[] nums, int target, int start, int end){
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]<=target){
+                start = mid+1;
+            }
+            else end = mid-1;
+        }
+        return nums[start]==target ? start : start-1;
     }
 }
