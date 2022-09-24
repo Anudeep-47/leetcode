2f40e463cbfe78b5d29e3ad30a846cc26191698a
Anudeep-47 <83406607+Anudeep-47@users.noreply.github.com>
2022-09-23 20:31:05 -0400

    Time: 13 ms (15.55%), Space: 57.1 MB (66.53%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,38 +1,33 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int lands = 0;
+        int m = grid.length, n = grid[0].length;
+        int count = 0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]=='1'){
-                    Queue<int[]> gridQueue = new ArrayDeque<>();
-                    lands++;
-                    grid[i][j] = '0';
-                    gridQueue.add(new int[]{i, j});
-                    while(!gridQueue.isEmpty()){
-                        int[] rc = gridQueue.poll();
-                        int r=rc[0], c=rc[1];
-                        if(r+1<m && grid[r+1][c]=='1'){
-                            gridQueue.add(new int[]{r+1, c});
-                            grid[r+1][c] = '0';
-                        }
-                        if(r>0 && grid[r-1][c]=='1'){
-                            gridQueue.add(new int[]{r-1, c});
-                            grid[r-1][c] = '0';
-                        }
-                        if(c+1<n && grid[r][c+1]=='1'){
-                            gridQueue.add(new int[]{r, c+1});
-                            grid[r][c+1] = '0';
-                        }
-                        if(c>0 && grid[r][c-1]=='1'){
-                            gridQueue.add(new int[]{r, c-1});
-                            grid[r][c-1] = '0';
-                        }
-                    }
+                    count++;
+                    bfs(grid, i, j);
                 }
             }
         }
-        return lands;
+        return count;
+    }
+    private void bfs(char[][] grid, int r, int c){
+        int m = grid.length, n = grid[0].length;
+        int[] dirs = new int[]{0, 1, 0, -1, 0};
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        grid[r][c] = '0';
+        queue.offer(new int[]{r, c});
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            for(int i=0; i<4; i++){
+                int nr = cur[0]+dirs[i];
+                int nc = cur[1]+dirs[i+1];
+                if(nr<0 || nr>=m || nc<0 || nc>=n || grid[nr][nc]!='1')continue;
+                grid[nr][nc] = '0';
+                queue.offer(new int[]{nr, nc});
+            }
+        }
+        return;
     }
 }
