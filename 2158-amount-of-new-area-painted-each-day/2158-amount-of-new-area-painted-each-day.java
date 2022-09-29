bc22634ddfb5b392ae769ecafbd5f9cb747c47a7
Anudeep-47 <83406607+Anudeep-47@users.noreply.github.com>
2022-09-29 13:44:20 -0400

    Time: 34 ms (71.87%), Space: 202.6 MB (48.02%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.java
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.java
@@ -1,18 +1,49 @@
 class Solution {
-    // jump line solution
-    public int[] amountPainted(int[][] paint) {
-        int[] line = new int[50001];
+    // // jump line solution
+    // public int[] amountPainted(int[][] paint) {
+    //     int[] line = new int[50001];
+    //     int[] res = new int[paint.length];
+    //     for(int i=0; i<paint.length; i++){
+    //         int start = paint[i][0];
+    //         int end = paint[i][1];
+    //         int amount = 0;
+    //         while(start < end){
+    //             int jump = Math.max(start+1, line[start]);
+    //             amount += line[start] == 0 ? 1 : 0;
+    //             line[start] = Math.max(line[start], end);
+    //             start = jump;
+    //         }
+    //         res[i] = amount;
+    //     }
+    //     return res;
+    // }
+    
+    // merge intervals with treemap
+    public int[] amountPainted(int[][] paint){
+        TreeMap<Integer, Integer> intervals = new TreeMap<>();
         int[] res = new int[paint.length];
         for(int i=0; i<paint.length; i++){
-            int start = paint[i][0];
-            int end = paint[i][1];
-            int amount = 0;
-            while(start < end){
-                int jump = Math.max(start+1, line[start]);
-                amount += line[start] == 0 ? 1 : 0;
-                line[start] = Math.max(line[start], end);
-                start = jump;
+            int[] cur = paint[i];
+            int start = cur[0];
+            int end = cur[1];
+            int amount = end-start;
+            Map.Entry<Integer, Integer> floor = intervals.floorEntry(cur[0]);
+            if(floor!=null){
+                if(floor.getValue()>=end)continue;
+                if(floor.getValue()>=start){
+                    amount -= floor.getValue()-start;
+                    intervals.remove(floor.getKey());
+                    start = floor.getKey();
+                }
             }
+            Map.Entry<Integer, Integer> ceil = intervals.ceilingEntry(cur[0]);
+            while(ceil!=null && ceil.getKey()<=end){
+                amount -= Math.min(end, ceil.getValue())-ceil.getKey();
+                intervals.remove(ceil.getKey());
+                end = Math.max(end, ceil.getValue());
+                ceil = intervals.ceilingEntry(cur[0]);
+            }
+            intervals.put(start, end);
             res[i] = amount;
         }
         return res;
