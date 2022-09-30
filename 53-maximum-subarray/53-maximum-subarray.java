55b79a258974d0802be03c8fe93c8ecee2a2ba53
Anudeep-47 <83406607+Anudeep-47@users.noreply.github.com>
2022-09-30 01:43:48 -0400

    Time: 22 ms (6.12%), Space: 73.5 MB (63.16%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,11 +1,23 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
-        int sum = 0;
-        for(int i=0; i<nums.length; i++){
-            sum = Math.max(sum+nums[i], nums[i]);
-            maxSum = Math.max(maxSum, sum);
+        return divideAndConquer(nums, 0, nums.length-1);
+    }
+    private int divideAndConquer(int[] nums, int left, int right){
+        if(left>right)return Integer.MIN_VALUE;
+        int mid = left + (right-left)/2;
+        int maxLeftSum = 0;
+        int maxRightSum = 0;
+        for(int i=mid-1, sum=0; i>=left; i--){
+            sum += nums[i];
+            maxLeftSum = Math.max(maxLeftSum, sum);
         }
-        return maxSum;
+        for(int i=mid+1, sum=0; i<=right; i++){
+            sum += nums[i];
+            maxRightSum = Math.max(maxRightSum, sum);
+        }
+        int curSum = maxLeftSum + nums[mid] + maxRightSum;
+        int leftSum = divideAndConquer(nums, left, mid-1);
+        int rightSum = divideAndConquer(nums, mid+1, right);
+        return Math.max(curSum, Math.max(leftSum, rightSum));
     }
 }
