e06f62040f9b07ff5017aa6d9fe99693611a09c9
Anudeep-47 <83406607+Anudeep-47@users.noreply.github.com>
2022-10-01 19:14:56 -0400

    Time: 2 ms (70.24%), Space: 42.4 MB (45.31%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -15,17 +15,32 @@ class Solution {
     //     return memo[i];
     // }
     
+    // public int numDecodings(String s){
+    //     int[] dp = new int[s.length()+1];
+    //     dp[0] = 1;
+    //     dp[1] = s.charAt(0)=='0' ? 0 : 1;
+    //     for(int i=2; i<dp.length; i++){
+    //         dp[i] = s.charAt(i-1)!='0' ? dp[i-1] : dp[i];
+    //         int val = Integer.parseInt(s.substring(i-2, i));
+    //         if(val >= 10 && val <= 26){
+    //             dp[i] += dp[i-2];
+    //         }
+    //     }
+    //     return dp[s.length()];
+    // }
+    
     public int numDecodings(String s){
-        int[] dp = new int[s.length()+1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0)=='0' ? 0 : 1;
-        for(int i=2; i<dp.length; i++){
-            dp[i] = s.charAt(i-1)!='0' ? dp[i-1] : dp[i];
-            int val = Integer.parseInt(s.substring(i-2, i));
-            if(val >= 10 && val <= 26){
-                dp[i] += dp[i-2];
+        int first = 1;
+        int second = s.charAt(0)=='0' ? 0 : 1;
+        for(int i=1; i<s.length(); i++){
+            int cur = s.charAt(i)!='0' ? second : 0;
+            int val = Integer.parseInt(s.substring(i-1, i+1));
+            if(val>=10 && val<=26){
+                cur += first;
             }
+            first = second;
+            second = cur;
         }
-        return dp[s.length()];
+        return second;
     }
 }
