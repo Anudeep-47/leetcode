20e6fd328e79f04ee666bb4ca36519391cb70ecb
Anudeep-47 <83406607+Anudeep-47@users.noreply.github.com>
2022-10-05 11:56:36 -0400

    Time: 37 ms (28.30%), Space: 75.9 MB (59.43%) - LeetHub

--- 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.java
+++ 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.java
@@ -1,13 +1,25 @@
 class Solution {
-    public int maxSumAfterOperation(int[] nums) {
-        int[][] dp = new int[nums.length][2];
-        dp[0][0] = nums[0];
-        dp[0][1] = nums[0]*nums[0];
-        int res = dp[0][1];
+    // public int maxSumAfterOperation(int[] nums) {
+    //     int[][] dp = new int[nums.length][2];
+    //     dp[0][0] = nums[0];
+    //     dp[0][1] = nums[0]*nums[0];
+    //     int res = dp[0][1];
+    //     for(int i=1; i<nums.length; i++){
+    //         dp[i][0] = Math.max(dp[i-1][0]+nums[i], nums[i]);
+    //         dp[i][1] = Math.max(nums[i]*nums[i], Math.max(dp[i-1][0]+nums[i]*nums[i], dp[i-1][1]+nums[i]));
+    //         res = Math.max(res, dp[i][1]);
+    //     }
+    //     return res;
+    // }
+    
+    public int maxSumAfterOperation(int[] nums){
+        int notSqrd = nums[0], sqrd = nums[0]*nums[0];
+        int res = sqrd;
         for(int i=1; i<nums.length; i++){
-            dp[i][0] = Math.max(dp[i-1][0]+nums[i], nums[i]);
-            dp[i][1] = Math.max(nums[i]*nums[i], Math.max(dp[i-1][0]+nums[i]*nums[i], dp[i-1][1]+nums[i]));
-            res = Math.max(res, dp[i][1]);
+            int nextNotSqrd = Math.max(notSqrd+nums[i], nums[i]);
+            sqrd = Math.max(nums[i]*nums[i], Math.max(notSqrd+nums[i]*nums[i], sqrd+nums[i]));
+            res = Math.max(res, sqrd);
+            notSqrd = nextNotSqrd;
         }
         return res;
     }
